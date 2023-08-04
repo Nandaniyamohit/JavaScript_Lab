8a9b9a354adc6edeaef28f284ec13d84cb1bb51b
Nandaniyamohit <141401523+Nandaniyamohit@users.noreply.github.com>
2023-08-04 15:20:42 +0000

    container done

---
+++ JS_Study/Var_And_Type.js
@@ -0,0 +1,17 @@
+// Print Data >>>>
+console.log("hello world");
+
+// Var Use >>>>
+const User_Id = 12221
+let name = "mohit"
+var name_1 = "ram" // not use because in var scope problem
+
+// Data Type >>>>
+// a). primitive              b). non_primitive
+// 1). number                 1). oblect
+// 2). big int                2). function
+// 3). string                 3). array
+// 4). boolen
+// 5). null  datatype object*
+// 6). undifine
+
